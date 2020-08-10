6968c13855ebb48ddc230be8646085818ae4d7b4
Mayu33 <58526412+Mayu33@users.noreply.github.com>
2020-08-10 12:36:35 -0300

    Add files via upload

---
+++ Aula01/Aula01/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StreamWriter texto = new StreamWriter(@"C:\Users\PC\Desktop\Aula01.txt");
+            texto.WriteLine("Atividade da Aula 01");
+            texto.Close();
+            Console.WriteLine("Arquivo criado com sucesso");
+            
+
+            StreamReader ler = new StreamReader(@"C:\Users\PC\Desktop\Aula01.txt", Encoding.UTF8);
+            string lido = ler.ReadToEnd();
+            Console.WriteLine(lido);
+            Console.ReadKey();
+        }
+    }
+}
